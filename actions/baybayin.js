f7b07783ec66062fd243d99c333d984bddd61d3f
Mr.RySes <mpoprev2@email.com>
2023-07-13 06:40:57 +0800

    07-13-23 06:40

---
+++ actions/baybayin.js
@@ -0,0 +1,96 @@
+const baybay = {
+	"A": 5888, "E": 5889, "O": 5890,
+	"b": 5898, "k": 5891, "d": 5895,
+	"g": 5892, "h": 5905, "l": 5902,
+	"m": 5899, "n": 5896, "ng": 5893,
+	"p": 5897, "s": 5904, "t": 5894,
+	"w": 5903, "y": 5900,
+	"e": 5906, "o": 5907, "a": 5908,
+	".": 5942, ",": 5941
+}
+let toString = (code) => {
+	return String.fromCharCode(code)
+}
+
+let toBaybayin = str => {
+	str = str.toLowerCase().replace(/r/gi, "d").replace(/i/gi, "e").replace(/u/gi, "o").replace(/x|z/gi, "s").replace(/c|q/gi, "k").replace(/j/gi, "dy").replace(/f/gi, "p").replace(/v/gi, "b")
+	let r = ""
+	for(let c = 0; c < str.length; c++){
+		if(c < str.length - 1){
+			if(str.charAt(c) == 'a'){
+				r += toString(baybay.A)
+			}else if(str.charAt(c) == 'e'){
+				r += toString(baybay.E)
+			}else if(str.charAt(c) == 'o'){
+				r += toString(baybay.O)
+			}else if(str.charAt(c) == "n" && str.charAt(c + 1) == "g"){
+				let d = str.charAt(c + 2)
+				if(str.length > 4){
+					if(str.charAt(c - 1) == " " && str.charAt(c + 2) == " "){
+						r += toString(baybay.n) + toString(baybay.ng) + toString(baybay.a)
+						c += 1
+					}else{
+						r += toString(baybay.ng)
+						if(d == 'e' && d != 'a'){
+							r += toString(baybay.e)
+						}else if(d == 'o' && d != 'a'){
+							r += toString(baybay.o)
+						}else if(d != 'a'){
+							r += toString(baybay.a)
+							c--
+						}
+						c += 2
+					}
+				}else{
+					r += toString(baybay.ng)
+					if(d == 'e' && d != 'a'){
+						r += toString(baybay.e)
+					}else if(d == 'o' && d != 'a'){
+						r += toString(baybay.o)
+					}else if(d != 'a'){
+						r += toString(baybay.a)
+						c--
+					}
+					c += 2
+				}
+			}else if(baybay[str.charAt(c)] == undefined && str.charAt(c) != 'a'){
+				r += str.charAt(c)
+			}else{
+				let d = str.charAt(c + 1)
+				r += toString(baybay[str.charAt(c)])
+				if(d == 'e' && d != 'a'){
+					r += toString(baybay.e)
+				}else if(d == 'o' && d != 'a'){
+					r += toString(baybay.o)
+				}else if(d != 'a'){
+					r += toString(baybay.a)
+					c--
+				}
+				c++
+			}
+		}else{
+			if(str.charAt(c) == 'a'){
+				r += toString(baybay.A)
+			}else if(str.charAt(c) == 'e'){
+				r += toString(baybay.E)
+			}else if(str.charAt(c) == 'o'){
+				r += toString(baybay.O)
+			}else{
+				r += toString(baybay[str.charAt(c)])
+				const lists = [
+					'b', 'k' ,'d', 'g', 'h',
+					'l',' m', 'n', 'p', 's',
+					't', 'w', 'y'
+				]
+				if(str.charAt(c).includes(lists)){
+					r += toString(baybay.a)
+				}
+			}
+		}
+	}
+	return r
+}
+
+module.exports = {
+	toBaybayin
+}
--- actions/verse.js
+++ actions/verse.js
@@ -1,13 +1,20 @@
 const axios = require("axios")
 const cheerio = require("cheerio")
+const baybayin = require("./baybayin")
 
-let search = async (verse, ver) => {
-	let { data } = await axios.get(`https://www.biblegateway.com/passage/?search=${verse}&version=${ver}`)
+let search = async (verse, ver = "") => {
+	let isBaybayin = ver.startsWith("baybay-")
+	let version = ver
+	if(isBaybayin){
+		version = ver.replace("baybay-", "")
+	}
+	console.log(ver)
+	let { data } = await axios.get(`https://www.biblegateway.com/passage/?search=${verse}&version=${version}`)
 	let $ = await cheerio.load(data)
 	let html = $(".passage-table")
 	let _ = []
 	html.each((i, e) => {
-		let base = $(e).find(`div[class='passage-col passage-col-mobile version-${ver}']`)
+		let base = $(e).find(`div[class='passage-col passage-col-mobile version-${version}']`)
 		let book = $(base).find(".dropdown-display-text")[0].children[0].data
 		let content = $(base).find(".std-text")
 		if(content.html() == null){
@@ -20,42 +27,46 @@ let search = async (verse, ver) => {
 			content = $(base).find(".text")
 		}
 		if(content.html() == null){
-			base = $(e).find(`div[class='version-${ver} result-text-style-normal text-html']`)
+			base = $(e).find(`div[class='version-${version} result-text-style-normal text-html']`)
 			content = base
 		}
 		let verse = ""
 		content.each((i, f) => {
-			verse += $(f).text() + "\n"
+			if(isBaybayin){
+				verse += baybayin.toBaybayin($(f).text()) + "\n"
+			}else{
+				verse += $(f).text() + "\n"
+			}
 		})
-		/*if(verse == ""){
-			base = $(e).find(`div[class='version-${ver} result-text-style-normal text-html']`)
-			content = $(base).find(".woj")
+		// if(verse == ""){
+		// 	base = $(e).find(`div[class='version-${ver} result-text-style-normal text-html']`)
+		// 	content = $(base).find(".woj")
 			
-			console.log("test ")
-			verse = ""
-		}
-		content.each((i, f) => {
-			verse += $(f).text() + "\n"
-		})
-		if(verse == ""){
-			base = $(e).find(`div[class='version-${ver} result-text-style-normal text-html']`)
-			content = $(base).find(".text")
-			verse = ""
-		}
-		content.each((i, f) => {
-			verse += $(f).text() + "\n"
-		})
-		if(verse == ""){
-			base = $(e).find(`div[class='version-${ver} result-text-style-normal text-html']`)
-			content = base
-			verse = ""
-		}
-		content.each((i, f) => {
-			verse += $(f).text() + "\n"
-		})*/
+		// 	console.log("test ")
+		// 	verse = ""
+		// }
+		// content.each((i, f) => {
+		// 	verse += $(f).text() + "\n"
+		// })
+		// if(verse == ""){
+		// 	base = $(e).find(`div[class='version-${ver} result-text-style-normal text-html']`)
+		// 	content = $(base).find(".text")
+		// 	verse = ""
+		// }
+		// content.each((i, f) => {
+		// 	verse += $(f).text() + "\n"
+		// })
+		// if(verse == ""){
+		// 	base = $(e).find(`div[class='version-${ver} result-text-style-normal text-html']`)
+		// 	content = base
+		// 	verse = ""
+		// }
+		// content.each((i, f) => {
+		// 	verse += $(f).text() + "\n"
+		// })
 		let json = {
 			"book": book,
-			"verse": verse
+			"verse": verse.replace(/\\x00/, "")
 		}
 		_.push(json)
 	})
